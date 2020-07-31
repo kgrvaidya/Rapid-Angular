1d1f96dc3337350926ccd8ac11c7adff3e76c314
K RamakrishnaVaidya <kravaidya@BLR.ALLEGISINDIA.COM>
2020-07-31 14:22:52 -0400

    Added libraries and navbar from template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ServicesComponent } from './components/services/services.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutUsComponent,
     ServicesComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
