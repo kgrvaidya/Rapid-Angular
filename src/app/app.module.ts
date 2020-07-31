e70705d886710a198f29a82fc0725c8565975a8e
K RamakrishnaVaidya <kravaidya@BLR.ALLEGISINDIA.COM>
2020-07-31 14:09:20 -0400

    Initialised Repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule
